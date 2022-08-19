2c608c3beebcf17129b7b45628c1481c1e988be9
Josh <joshua87@usf.edu>
2022-08-19 07:55:11 -0400

    sped up the the way random images are retrieved tremendously by saving the json of the images if it doesnt exist, if it does then read from saved json

--- actions/randomimage.ts
+++ actions/randomimage.ts
@@ -5,9 +5,26 @@ module.exports = {
         //send the message "finding your message now"
         msg.channel.send("finding your image now :3");
 
+        //check if directory exists
+        if(require('fs').existsSync('data/' + msg.channel.id)){
+            //if it does then read the file
+            let messages = readFile(msg);
+
+            //console.log("messages read");
+            //console.log(messages);
+
+            //send the message
+            sendRandomMessage(msg,messages);
+        }
+        else{
+            //if it doesn't then create the file
+            fetchAllImages(msg);
+        }
+
+
 
         
-        fetchAllImages(msg);
+        
     }
 
     
@@ -49,14 +66,17 @@ async function fetchAllImages(msg) {
             );
         }
 
-        console.log("messages fetched");
-        console.log(messages);
+        //console.log("messages fetched");
+        //console.log(messages);   
 
-        let randomMessage = messages[Math.floor(Math.random() * messages.length)];
+        //create the file if it doesn't exist
+        createFile(msg,messages);
 
-        let messageAuthor = randomMessage.author.username;
+        //read the file
+        messages = readFile(msg);
 
-        msg.channel.send(messageAuthor + ': ' + randomMessage.attachments.first().url);    
+        //send the message
+        sendRandomMessage(msg,messages);
     }
     catch(err){
       //send message that the command crashed
@@ -64,5 +84,71 @@ async function fetchAllImages(msg) {
       msg.channel.send("I crashed! Owie! Let me know if you see this message! :3");
       console.log(err);
     }
+}
 
-  }
+function sendRandomMessage(msg,messages){
+
+     //console.log("messages read");
+    //console.log(messages);
+
+    //get number of keys in the messages
+    let numKeys = Object.keys(messages).length - 1;
+    //console.log(numKeys);
+
+    //get a random number between 0 and the length of the array
+    let randomNumber = Math.floor(Math.random() * numKeys).toString();
+
+    //send the message with author and image
+    msg.channel.send(messages[randomNumber].author + ": " + messages[randomNumber].image);
+}
+
+function createFile(msg, messages){
+
+    //create a new array size of messages array
+    let newMessages = new Array(messages.length);
+
+    //console.log(messages[0].attachments);
+    //console.log(msg.author.username);
+    
+    //loop through the messages and add them to the new object
+    for(let i = 0; i < messages.length; ++i){
+        newMessages[i] = {
+            "author": msg.author.username,
+            "image": messages[i].attachments.first().url,
+            "channelId": msg.channel.id
+        }
+    }
+
+
+    var rv = {};
+    for (var i = 0; i < newMessages.length; ++i)
+        rv[i] = newMessages[i];
+
+    require('fs').mkdir(
+
+        'data/' + newMessages[0].channelId,
+
+        { recursive: true },
+
+        function (err) {
+            if (err) throw err;
+            console.log('Directory created successfully!');
+        }
+    );
+
+    require('fs').writeFileSync('data/' + newMessages[0].channelId + '/images.json', JSON.stringify(rv), 'utf8');
+    
+}
+
+function readFile(msg){
+
+    let messages = require('fs').readFileSync('data/' + msg.channel.id + '/images.json', function (err) {
+        console.log('complete');
+    }).toString();
+
+    Object.entries(JSON.parse(messages)).forEach( ([key, value]) => {
+        messages[key] = value;
+    } );
+
+    return JSON.parse(messages);
+}
